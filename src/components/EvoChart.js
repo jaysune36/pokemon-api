de95dbd6f967326db629967a1a88028505b0a5ab
user <user@users-MacBook-Pro.local>
2025-09-22 15:15:13 -0700

    added more to traverseObject function to push data to the evoChainArr so it can be displayed on the evolution page, this includes levels up or trade data, working to have more detailed information displayed.

--- src/components/EvoChart.js
+++ src/components/EvoChart.js
@@ -13,7 +13,14 @@ function EvoChart({evoChain}) {
         if(typeof value === 'object' && value !== null) {
           if(key === 'evolves_to' && value[0] !== undefined) {
             for(let i=0; i<value.length; i++) {
-              console.log(value[i].species.name);
+              console.log(value[i].evolution_details);
+              evoChainArr.push(<p>Evolves To: {value[i].species.name}</p>)
+              if(value[i].evolution_details[0].min_level) {
+                evoChainArr.push(<p>@{value[i].evolution_details[0].min_level}</p>)
+              } else if (value[i].evolution_details[0].trigger) {
+                evoChainArr.push(<p>@{value[i].evolution_details[0].trigger.name}</p>)
+              }
+    
             }
             
             // console.log(value);
@@ -37,20 +44,14 @@ function EvoChart({evoChain}) {
   g: [4, 5, { h: 6 }]
 };
 
+traverseObject(evoChain.chain)
+
   return (
     <div className='d-block-flex flex-row justify-content-center a mt-2 mb-2'>
       <h6 className='text-center'>Evolution Chart</h6>
-      {/* {
-        evoChartLoading ? (
-          <p>loading...</p>
-        ) : ( */}
-        <p onClick={()=>traverseObject(evoChain.chain)}>{evoChain.chain.species.name}</p>
+     <p onClick={()=>traverseObject(evoChain.chain)}>{evoChain.chain.species.name}</p>
         {evoChainArr}
-         {/* <p>{evoChain.chain.species.name}</p>
-         <p>{evoChain.chain.evolves_to[0].species.name}</p>
-         <p>{evoChain.chain.evolves_to[0].evolves_to[0].species.name}</p> */}
-        {/* // ) */}
-      {/* } */}
+     
     </div>
   )
 }
